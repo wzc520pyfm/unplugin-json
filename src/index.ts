97515d338acb7b2751b906c7d340cc55186093a9
wzc520pyfm <1528857653@qq.com>
2024-08-05 21:15:21 +0800

    feat: add json resolver

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,40 @@
+import { createFilter, dataToEsm } from '@rollup/pluginutils'
 import type { UnpluginFactory } from 'unplugin'
 import { createUnplugin } from 'unplugin'
 import type { Options } from './types'
 
-export const unpluginFactory: UnpluginFactory<Options | undefined> = options => ({
-  name: 'unplugin-starter',
-  transformInclude(id) {
-    return id.endsWith('main.ts')
-  },
-  transform(code) {
-    return code.replace('__UNPLUGIN__', `Hello Unplugin! ${options}`)
-  },
-})
+export const unpluginFactory: UnpluginFactory<Options | undefined> = (options = {}) => {
+  const filter = createFilter(options.include, options.exclude)
+  const indent = 'indent' in options ? options.indent : '\t'
+
+  return {
+    name: 'unplugin-json',
+    transform(code, id) {
+      if (id.slice(-5) !== '.json' || !filter(id))
+        return null
+
+      try {
+        const parsed = JSON.parse(code)
+        return {
+          code: dataToEsm(parsed, {
+            preferConst: options.preferConst,
+            compact: options.compact,
+            namedExports: options.namedExports,
+            includeArbitraryNames: options.includeArbitraryNames,
+            indent,
+          }),
+          map: { mappings: '' },
+        }
+      }
+      catch (err) {
+        const message = 'Could not parse JSON file'
+        // FIXME remove any
+        this.error({ message, id, cause: err } as any)
+        return null
+      }
+    },
+  }
+}
 
 export const unplugin = /* #__PURE__ */ createUnplugin(unpluginFactory)
 
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,15 @@
+/**
+ * A valid `picomatch` glob pattern, or array of patterns.
+ */
+export type FilterPattern = ReadonlyArray<string | RegExp> | string | RegExp | null
+
 export interface Options {
   // define your plugin options here
+  include?: FilterPattern
+  exclude?: FilterPattern
+  compact?: boolean
+  includeArbitraryNames?: boolean
+  indent?: string
+  namedExports?: boolean
+  preferConst?: boolean
 }
